240846bc3af0b26f4977db7682727a6ebc2af5d8
Elmer Huitz <elmohuitz@gmail.com>
2023-03-04 16:55:57 +0100

    showing cards in front end

---
+++ frontend/src/components/UserCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="group relative">
+    <div
+      class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-50">
+      <img :src="weatherIconUrl" alt="weather icon"
+        class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+    </div>
+    <div class="mt-4 flex justify-between">
+      <div>
+        <h3 class="text-sm text-gray-700">
+          <a href="#">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            {{ user.name }}
+          </a>
+        </h3>
+        <p class="mt-1 text-sm text-gray-500">{{ user.email }}</p>
+      </div>
+      <!-- <p class="text-sm font-medium text-gray-900">{{ user.weathers[0].weather.properties.periods[0].icon }}</p> -->
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  name: 'UserCard',
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    weatherIconUrl() {
+      return this.user.weathers[0].weather.properties.periods[0].icon;
+    }
+  }
+};
+</script>
---
+++ frontend/src/components/UserList.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div class="bg-white">
+  <div class="mx-auto max-w-xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-5xl lg:px-8">
+    <h2 class="text-2xl font-bold tracking-tight text-gray-900">User - Weather</h2>
+
+    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-20 sm:grid-cols-2 lg:grid-cols-5 xl:gap-x-8">
+      
+      <user-card v-for="user in users" :key="user.id" :user="user" />
+     
+    </div>
+  </div>
+
+</div>
+
+</template>
+  
+<script>
+import axios from 'axios';
+import UserCard from './UserCard.vue';
+
+export default {
+  name: 'UserList',
+  components: {
+    UserCard,
+  },
+  data() {
+    return {
+      users: [],
+      isLoading: true,
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('http://localhost/');
+      this.users = response.data.users;
+
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.isLoading = false;
+    }
+  },
+};
+</script>
+  
